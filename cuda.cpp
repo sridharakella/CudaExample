81c62c959fd3cb86acbd57d46c6b4fc09b45e660
Sridhar <akella.sridhar@gmail.com>
2024-11-26 13:15:05 -0500

    Update cuda.cpp
    This is vector additon and memory allocation bewteeen GPU and CPU

--- cuda.cpp
+++ cuda.cpp
@@ -1,9 +1,9 @@
 # <include stdio.h>
-# <include cuda_runtime.sh>
+# <include cuda_runtime.sh> // vector addition
 
 #define SIZE 1024;
 
-__global_ void vectorAdd(int* A, int* B, int* C, int n)
+__global_ void vectorAdd(int* A, int* B, int* C, int n) // this method will be called by GPU
 {
     int i=threadIdx.x;
     C[i]= A[i]+ B[i];
@@ -15,7 +15,7 @@ int * A, * B, * C; // host vector
 int *d_A, * d_B, * d_C; // Device vectors 
 int size= SIZE * sizeof(int)
 
-A = (int*)malloc(size);
+A = (int*)malloc(size); // allocating memory
 B = (int*)malloc(size);
 c= (int *)malloc(size);
 
@@ -28,12 +28,12 @@ for (int i=0 i<size; i++)
     A[i]=i;
     B[i]=SIZE-i;
 }
-cudaMemCpy(d_A,A,size,cudaMemCpyHostToDevice);
-cudaMemCpy(d_B,B,size,cudaMemCpyHostToDevice);
+cudaMemCpy(d_A,A,size,cudaMemCpyHostToDevice); // cudamemcpy Host is CPU and device is GPU
+cudaMemCpy(d_B,B,size,cudaMemCpyHostToDevice); 
 
 vectorAdd << <1,1024 >> > (d_A,d_b, d_C)
 
-cudaMemCpy(C, d_C, size, cudaMemcpyDeviceToHost)
+cudaMemCpy(C, d_C, size, cudaMemcpyDeviceToHost); // once the vector addition is completed, copying from GPU to CPU
 
 for(int i=0; i< SIZE; i++)
 {
